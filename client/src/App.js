979c6d6a7a94659d7aef89d814f89912f6164b86
Victoriajuszkiewicz <wanderer.foxy@gmail.com>
2023-03-27 15:19:47 +0200

    routes written

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import Homepage from "./Pages/Homepage/Homepage.js";
+import Movies from "./Pages/Movies/Movies.js";
+import Series from "./Pages/Series/Series.js";
+import Trending from "./Pages/Trending/Trending.js";
+import Login from "./Pages/Login/Login.js";
+import Register from "./Pages/Register/Register.js";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Homepage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
